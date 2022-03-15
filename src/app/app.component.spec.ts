fe858a26584d01d46059205c2ad20d48c1b188c0
Denis Prokhorenko <denis07011997q@gmail.com>
2022-03-15 22:01:12 +0300

    refactor: apply eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'youtube-client'`, () => {
+  it('should have as title \'youtube-client\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('youtube-client');
