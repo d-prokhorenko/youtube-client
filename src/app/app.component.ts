3a7da2472502650d8532569b78e67b3a74b16a6b
Denis Prokhorenko <denis07011997q@gmail.com>
2022-03-27 11:53:31 +0300

    feat: add sorting by date and views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, DoCheck } from '@angular/core';
 
 import { response } from './response.mock';
 import { Response } from './interfaces/response';
+import { Video } from './interfaces/response';
 import { IsFilterVisibleService } from './services/is-filter-visible.service';
 
 @Component({
@@ -21,5 +22,37 @@ export class AppComponent implements DoCheck {
   searchItems(value: string) {
     console.log(value);
     this.data = response;
+  }
+
+  filterByDate(isAscending: boolean) {
+    if (isAscending) {
+      this.data.items = this.data.items.sort(
+        (item1: Video, item2: Video) =>
+          new Date(item1.snippet.publishedAt).getTime() -
+          new Date(item2.snippet.publishedAt).getTime()
+      );
+    } else {
+      this.data.items = this.data.items.sort(
+        (item1: Video, item2: Video) =>
+          new Date(item2.snippet.publishedAt).getTime() -
+          new Date(item1.snippet.publishedAt).getTime()
+      );
+    }
+  }
+
+  filterByViews(isAscending: boolean) {
+    if (isAscending) {
+      this.data.items = this.data.items.sort(
+        (item1: Video, item2: Video) =>
+          Number(item1.statistics.viewCount) -
+          Number(item2.statistics.viewCount)
+      );
+    } else {
+      this.data.items = this.data.items.sort(
+        (item1: Video, item2: Video) =>
+          Number(item2.statistics.viewCount) -
+          Number(item1.statistics.viewCount)
+      );
+    }
   }
 }
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent {
+  isDateAscedning: boolean | null = null;
 
-  constructor() { }
+  isViewsAscedning: boolean | null = null;
 
-  ngOnInit(): void {
+  @Output() dateFilter = new EventEmitter<boolean>();
+
+  @Output() viewsFilter = new EventEmitter<boolean>();
+
+  onDate(): void {
+    this.isDateAscedning = !this.isDateAscedning;
+    this.isViewsAscedning = null;
+    this.dateFilter.emit(this.isDateAscedning);
   }
 
+  onViews(): void {
+    this.isViewsAscedning = !this.isViewsAscedning;
+    this.isDateAscedning = null;
+    this.viewsFilter.emit(this.isViewsAscedning);
+  }
 }
