d5560054fdf3fe5f42cc106d16bf01c2c867b6dd
Denis Prokhorenko <denis07011997q@gmail.com>
2022-03-29 14:39:58 +0300

    fix: do not permit use filter buttons if data does not exist

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -14,18 +14,25 @@ export class FilterComponent {
   constructor(private dataService: DataService) {}
 
   onDate(): void {
+    if (!this.checkData()) return;
     this.isDateAscedning = !this.isDateAscedning;
     this.dataService.filterByDate(this.isDateAscedning);
     this.isViewsAscedning = null;
   }
 
   onViews(): void {
+    if (!this.checkData()) return;
     this.isViewsAscedning = !this.isViewsAscedning;
     this.dataService.filterByViews(this.isViewsAscedning);
     this.isDateAscedning = null;
   }
 
   onSearch(value: string): void {
+    if (!this.checkData()) return;
     this.dataService.filterBySearch(value);
+  }
+
+  checkData(): boolean {
+    return !!this.dataService.data;
   }
 }
