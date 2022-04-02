b5bbe367ef08e19085dd77f9a6c8f4bafbcae4a4
Denis Prokhorenko <denis07011997q@gmail.com>
2022-04-02 15:44:23 +0300

    refactor: remove unnecessary code form youtube-routing module

--- src/app/youtube/youtube-routing.module.ts
+++ src/app/youtube/youtube-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ItemDetailedComponent } from './pages/item-detailed/item-detailed.component';
 
-const routes: Routes = [
-  { path: 'search/:id', component: ItemDetailedComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
