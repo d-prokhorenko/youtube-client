e8e7a3edaa2028d5e3c34b6ac958b8fa430c5274
Denis Prokhorenko <denis07011997q@gmail.com>
2022-04-06 20:07:33 +0300

    feat: add login form and its validation

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { MyValidator } from '../../validators/my.validators';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+
   constructor(private authSerivce: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        MyValidator.password,
+      ]),
+    });
+  }
+
+  submit(): void {
+    console.log(this.form);
+  }
 
   login(): void {
     this.authSerivce.login();
---
+++ src/app/auth/validators/my.validators.ts
@@ -0,0 +1,34 @@
+import { FormControl } from '@angular/forms';
+
+interface Errors {
+  length?: string;
+  upperLower?: string;
+  letNum?: string;
+  special?: string;
+}
+
+export class MyValidator {
+  static password(control: FormControl): Errors | null {
+    const errors: Errors = {};
+    if (control.value.length < 8) {
+      errors.length = '- at least 8 characters';
+    }
+    if (
+      !control.value.match(new RegExp('[A-Z]')) ||
+      !control.value.match(new RegExp('[a-z]'))
+    ) {
+      errors.upperLower = '- a mixture of both uppercase and lowercase letters';
+    }
+    if (
+      !control.value.match(new RegExp('[0-9]')) ||
+      !control.value.match(new RegExp('[A-Fa-f]'))
+    ) {
+      errors.letNum = '- a mixture of letters and numbers';
+    }
+    if (!control.value.match(new RegExp('[\\,\\!\\@\\#\\?\\]]'))) {
+      errors.special =
+        '- inclusion of at least one special character, e.g., ! @ # ? ]';
+    }
+    return Object.keys(errors).length == 0 ? null : errors;
+  }
+}
