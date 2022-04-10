d5cf109fbe0d4cd14ed6c5a4636bf082070f3ac3
Denis Prokhorenko <denis07011997q@gmail.com>
2022-04-10 15:12:19 +0300

    feat: add some code for search videos reducer

--- src/app/core/components/header/search-with-button/search-with-button.component.ts
+++ src/app/core/components/header/search-with-button/search-with-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { fromEvent } from 'rxjs';
 import {
   map,
@@ -8,6 +9,8 @@ import {
   filter,
 } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { SearchVideos } from 'src/app/redux/actions/search.action';
+import { YoutubeState } from 'src/app/redux/state.model';
 
 import { DataService } from 'src/app/youtube/services/data.service';
 
@@ -23,7 +26,8 @@ export class SearchWithButtonComponent implements OnInit {
   constructor(
     private dataService: DataService,
     private router: Router,
-    private auth: AuthService
+    private auth: AuthService,
+    private store: Store<YoutubeState>
   ) {}
 
   ngOnInit(): void {
@@ -36,6 +40,7 @@ export class SearchWithButtonComponent implements OnInit {
       )
       .subscribe((value) => {
         this.dataService.getData(value);
+        this.store.dispatch(new SearchVideos(value));
         this.router.navigate(['search']);
       });
   }
